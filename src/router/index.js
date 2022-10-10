107935a9bdb4e67d0ce4b4f905f9701005b69825
khanhcp891 <77950148+khanhcp891@users.noreply.github.com>
2022-10-10 08:50:38 +0700

    Add project tiktok first

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { HeaderOnly } from '~/components/Layout';
+
+import Following from '~/pages/Following';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+const publicRouter = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRouter = [];
+
+export { publicRouter, privateRouter };
